7fa316a972fdf9f474be7f3eabcccea3d77f08f7
Cesar Freitez <cesar.freitez@chepss.cl>
2023-01-06 20:23:35 +0000

    [ADD] Creación del módulo de integración Odoo - Alfresco

---
+++ alfresco-connector/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Conector Alfresco',
+    'summary': """Módulo de conexion Alfresco - Odoo"""
+    'description': """
+        Connector Alfresco - Odoo
+    """,
+    'author': 'CHEPSS',
+    'website': 'https://www.chepss.cl',
+    'category': 'INTEGRACIONES',
+    'version': '0.1',
+    'depends': [],
+    'license': 'LGPL-3',
+    'data':[],
+    'demo':[],
+}
